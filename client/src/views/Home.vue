b4085d225010dcbc98fe4701b2027eb041be6036
Tanel Tekko <taneldeviis@hotmail.com>
2021-05-20 16:45:47 +0300

    Route list prototype

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,26 @@
 <template>
+<div>
+  <div class="planetList">
+    <v-list rounded>
+      <v-subheader>Select start route</v-subheader>
+      <v-list-item-group
+        v-model="selectedItem"
+        color="primary"
+      >
+        <v-list-item
+          v-for="(destination, i) in destinations"
+          :key="i"
+        >
+          
+          <v-list-item-content>
+            <v-list-item-title v-text="destination.name"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </div>
   <solar-system />
+</div>
 </template>
 
 <script>
@@ -7,7 +28,20 @@ import SolarSystem from '../components/SolarSystem.vue'
 
   export default {
     name: 'Home',
-
+    data: () => ({
+      selectedItem: 1,
+      destinations: [
+        {name: "Earth"},
+        {name: "Mercury"},
+        {name: "Venus"},
+        {name: "Mars"},
+        {name: "Jupiter"},
+        {name: "Saturn"},
+        {name: "Uranus"},
+        {name: "Neptune"},
+        
+      ]
+    }),
     components: {
       SolarSystem,
     },
