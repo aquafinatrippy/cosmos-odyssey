5312eca6d29470911e17013232221eb3910bbd1d
Tanel Tekko <taneldeviis@hotmail.com>
2021-05-23 09:01:14 +0300

    Component for single planet

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,17 +1,25 @@
 <template>
-  <v-container>
-  
-  </v-container>
+  <div class="solarSystem">
+    <Planet
+      PlanetTexture="https://st2.depositphotos.com/7671398/10659/i/950/depositphotos_106592906-stock-photo-red-planet-mars-texture.jpg"
+    />
+    <br>
+    <Planet
+    PlanetTexture="https://web.cortland.edu/flteach/civ/davidweb/images/earthmab.jpg"
+     />
+  </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
+import Vue from "vue";
+import Planet from "./Planet.vue";
 
-  export default Vue.extend({
-    name: 'HelloWorld',
+export default Vue.extend({
+  name: "HelloWorld",
 
-    data: () => ({
-     
-    }),
-  })
+  data: () => ({}),
+  components: {
+    Planet,
+  },
+});
 </script>
---
+++ client/src/components/Planet.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="planet">
+    <div class="wrap">
+      <div
+        class="background"
+        :style="` background: url(${PlanetTexture})repeat-x; `"
+      ></div>
+      <div class="clouds"></div>
+    </div>
+    <div class="mask"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "Planet",
+  props: ["PlanetTexture"],
+  mounted() {
+    console.log(this.PlanetTexture);
+  },
+});
+</script>
