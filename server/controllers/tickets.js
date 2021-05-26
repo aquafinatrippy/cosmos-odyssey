1e1127fef5c84845a806da18086597a825cde07e
aquafinatrippy <taneldeviis@hotmail.com>
2021-05-26 16:21:23 +0300

    backend build to mvc

---
+++ server/controllers/tickets.js
@@ -0,0 +1,10 @@
+module.exports = {
+	tickets: async (req, res) => {
+        try {
+          const reservations = await Reservation.find({});
+          res.send(reservations);
+        } catch (error) {
+          res.status(400).send(error);
+        }
+      }
+};
