c19753bfaa42bf0f5e33415166a588193d3bf46b
aquafinatrippy <taneldeviis@hotmail.com>
2021-05-27 10:21:17 +0300

    very simple e2e test

--- client/tests/e2e/specs/test.js
+++ client/tests/e2e/specs/test.js
@@ -1,8 +1,31 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
-  })
-})
+describe("Pages visiting", () => {
+  it("Visits the app root url", () => {
+    cy.visit("/");
+    cy.contains("h3", "Select start destination");
+  });
+  // it("visit the about page", () => {
+  //   cy.visit("/");
+  //   cy.contains("h1", "About page");
+  // })
+});
+
+describe("Items selecting and deselecting", () => {
+  it("Select first planet", () => {
+    cy.get("#mercury").click();
+  });
+  it("Select second planet", () => {
+    cy.get("#venus").click();
+  });
+  it("Should deselect second planet", () => {
+    cy.get("span")
+      .contains("venus")
+      .children("button")
+      .click();
+  });
+  it("Select second planet again", () => {
+    cy.get("#venus").click();
+  });
+
+});
