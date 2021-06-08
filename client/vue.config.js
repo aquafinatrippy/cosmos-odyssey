be02d3377951ca4477be4113d0734ca541d67664
aquafinatrippy <taneldeviis@hotmail.com>
2021-06-08 21:25:08 +0300

    Local docker DB support added

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   outputDir: path.resolve(__dirname, "../server/public"),
   devServer: {
-    proxy: "http://localhost:3000",
+    proxy: "http://backend-docker:3000",
   },
   transpileDependencies: ["vuetify"],
 };
